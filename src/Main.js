63cd7e0d8c7c0f9b9fc957a926c2690a10443fe5
sebiny <rucy8888@naver.com>
2025-03-06 14:24:33 +0900

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./components/App";
+import HomePage from "./pages/HomePage";
+import CourseListPage from "./pages/CourseListPage";
+import CoursePage from "./pages/CoursePage";
+import WishlistPage from "./pages/WishlistPage";
+import QuestionListPage from "./pages/QuestionListPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="courses">
+            <Route index element={<CourseListPage />} />
+            <Route path=":courseSlug" element={<CoursePage />} />
+          </Route>
+          <Route path="wishlist" element={<WishlistPage />} />
+          <Route path="questions" element={<QuestionListPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Main;
---
+++ src/components/Nav.js
@@ -0,0 +1,41 @@
+import Container from "./Container";
+import UserMenu from "./UserMenu";
+import logoImg from "../assets/logo.svg";
+import styles from "./Nav.module.css";
+import { Link, NavLink } from "react-router-dom";
+
+const getLinkStyle = ({ isActive }) => {
+  // 현재 경로가 to에 써있는 경로와 같으면 true
+  return {
+    textDecoration: isActive ? "underline" : undefined,
+  };
+};
+
+function Nav() {
+  return (
+    <div className={styles.nav}>
+      <Container className={styles.container}>
+        <Link to="/">
+          <img src={logoImg} alt="Codethat Logo" />
+        </Link>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to="/courses" style={getLinkStyle}>
+              카탈로그
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/questions" style={getLinkStyle}>
+              커뮤니티
+            </NavLink>
+          </li>
+          <li>
+            <UserMenu />
+          </li>
+        </ul>
+      </Container>
+    </div>
+  );
+}
+
+export default Nav;
